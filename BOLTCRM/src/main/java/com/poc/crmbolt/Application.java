e25e465689de0e32eb83c8c96cb259c6bf5841d1
Suvrat2014 <36414054+Suvrat2014@users.noreply.github.com>
2020-01-21 20:55:17 +0530

    Update Application.java

--- BOLTCRM/src/main/java/com/poc/crmbolt/Application.java
+++ BOLTCRM/src/main/java/com/poc/crmbolt/Application.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
   
 @SpringBootApplication
-public class Application{
-  
+public class Application extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(applicationClass, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<Application> applicationClass = Application.class;
+}
 }
